b77d31dc4620b8828f80021fe9ba614864b80f1b
bencallis1 <bencallis1@gmail.com>
2016-06-02 16:53:15 -0600

    Added Ionic and Angular to the project

---
+++ client/scripts/routes.js
@@ -0,0 +1,27 @@
+/**
+ * Created by Ben on 6/2/16.
+ */
+import { Config } from 'angular-ecmascript/module-helpers';
+
+export default class RoutesConfig extends Config {
+    configure() {
+        this.$stateProvider
+            .state('tab', {
+                url: '/tab',
+                abstract: true,
+                templateUrl: 'client/templates/tabs.html'
+            })
+            .state('tab.chats', {
+                url: '/chats',
+                views: {
+                    'tab-chats': {
+                        templateUrl: 'client/templates/chats.html'
+                    }
+                }
+            });
+
+        this.$urlRouterProvider.otherwise('tab/chats');
+    }
+}
+
+RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
